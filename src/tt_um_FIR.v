a64048fa6d65e881549918f3e8f99ba6bc9c247b
MrMisterial <markus.philip1999@gmail.com>
2023-12-19 20:40:38 +0100

    commented out3

--- src/tt_um_FIR.v
+++ src/tt_um_FIR.v
@@ -30,33 +30,16 @@ module tt_um_FIR #( parameter MAX_COUNT = 24'd10_000_000 ) (
     // otherwise use the hard coded MAX_COUNT
     wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
 
-	/*
+	
     always @(posedge clk) begin
         // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
+       
+        second_counter <= 0;
+        digit <= 0;
+        
     end
-    */
     
-    assign digit = 4'b0000;
+
 
     // instantiate segment display
     seg7 seg7(.counter(digit), .segments(led_out));
