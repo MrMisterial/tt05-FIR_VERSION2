66974cf0fd66564de11e855eab3ae3acdf04886d
MrMisterial <markus.philip1999@gmail.com>
2023-12-20 09:17:43 +0100

    include commented out

--- src/tt_um_FIR.v
+++ src/tt_um_FIR.v
@@ -1,5 +1,4 @@
 `default_nettype none
-`include "fir.v"
 
 module tt_um_FIR #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
