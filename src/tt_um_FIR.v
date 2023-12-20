d5aeb123e530289795be03924241016faa10679e
MrMisterial <markus.philip1999@gmail.com>
2023-12-20 08:54:26 +0100

    every output set

--- src/tt_um_FIR.v
+++ src/tt_um_FIR.v
@@ -13,8 +13,10 @@ module tt_um_FIR #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     wire reset = ! rst_n;
     wire [6:0] led_out;
-    //assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
+    assign uo_out[7:0] = 8'b00000000;
+    assign uio_out[7:0] = 8'b00000000;
+    assign uio_oe[7:0] = 8'b11111111;
+    //assign uo_out[7] = 1'b0;
 
     // use bidirectionals as outputs
     //assign uio_oe = 8'b11111111;
