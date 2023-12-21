b5451913a991a762042d172d984ab9ad334e9f5d
MrMisterial <markus.philip1999@gmail.com>
2023-12-21 18:39:13 +0100

    reduce tap size

--- src/fir_adapt_koef.v
+++ src/fir_adapt_koef.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module FIR #(
-	parameter TAP_SIZE = 6,
+	parameter TAP_SIZE = 2,
 	parameter NBR_OF_TAPS = 3,
 	parameter X_N_SIZE = 8,
 	parameter Y_N_SIZE = 14 //TAP_SIZE + X_N_SIZE minimum
