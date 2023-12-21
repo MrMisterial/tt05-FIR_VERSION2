d292d9c9228946cfa5d4db38aa6d154337601892
MrMisterial <markus.philip1999@gmail.com>
2023-12-21 21:26:48 +0100

    tapsize set to 3 again

--- src/fir_adapt_koef.v
+++ src/fir_adapt_koef.v
@@ -2,7 +2,7 @@
 
 module FIR #(
 	parameter TAP_SIZE = 3,
-	parameter NBR_OF_TAPS = 4,
+	parameter NBR_OF_TAPS = 3,
 	parameter X_N_SIZE = 8,
 	parameter Y_N_SIZE = 11 //TAP_SIZE + X_N_SIZE minimum
 ) (
