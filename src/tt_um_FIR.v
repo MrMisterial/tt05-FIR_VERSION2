e11beda8e26b8f800afab91c703276d2fcbbd565
MrMisterial <markus.philip1999@gmail.com>
2023-12-21 10:32:49 +0100

    output set

--- src/tt_um_FIR.v
+++ src/tt_um_FIR.v
@@ -15,7 +15,7 @@ module tt_um_FIR #( parameter MAX_COUNT = 24'd10_000_000 ) (
     //wire [6:0] led_out;
     //assign uo_out[7:0] = 8'b00000000;
     //assign uio_out[7:0] = 8'b00000000;
-    assign uio_oe[7:0] = 8'b00111111;
+    assign uio_oe[7:0] = 8'b11111100;
     //assign uo_out[7] = 1'b0;
 
     // use bidirectionals as outputs
